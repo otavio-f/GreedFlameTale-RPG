3982834b3b903db2efba7fe047932af1929db9ae
otavio-f <38397566+otavio-f@users.noreply.github.com>
2022-02-24 10:25:55 -0300

    Arquivos iniciais

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greed_Flame_Tale_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
